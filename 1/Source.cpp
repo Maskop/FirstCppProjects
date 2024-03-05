27d61c61072178252991ab3b7165a36a83f5254a
Joachim Otáhal <jachym.otahal@gmail.com>
2024-03-05 18:04:03 +0100

    Calculator added

--- 1/Source.cpp
+++ 1/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 void helloWorld() {
@@ -20,10 +21,46 @@ void WriteOutFloat() {
 	cin.get();
 }
 
+void RepeatString() {
+	cout << "Ahoj, jsem virtualni papousek Lora, rad opakuji!" << endl;
+	cout << "Napis neco: " << endl;
+	string input;
+	cin >> input;
+	string output;
+	output = input + "," + input + "!";
+	cout << output << endl;
+	cin.get(); cin.get();
+}
+
+void Multiplyer() {
+	int a, b;
+	cout << "Zadejte dve cisla: " << endl;
+	cin >> a >> b;
+	cout << "Vysledek je: " << a * b << endl;
+	cin.get(); cin.get();
+}
+
+void Calculator() {
+	float a, b;
+	cout << "Enter two numbers(floating points) with a space." << endl;
+	cin >> a >> b;
+	float multiply = a * b;
+	float divide = a / b;
+	float add = a + b;
+	float subtract = a - b;
+	cout << "Multiply: " << multiply << endl;
+	cout << "Divide: " << divide << endl;
+	cout << "Add: " << add << endl;
+	cout << "Subtract: " << subtract << endl;
+	cin.get(); cin.get();
+}
+
 int main(void) {
 	//helloWorld();
 	//WriteOutInt();
 	//WriteOutFloat();
-
+	//RepeatString();
+	//Multiplyer();
+	Calculator();
 	return 0;
 }
